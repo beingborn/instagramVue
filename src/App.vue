e76163a22265c73147c9a75c25ac400dfc08868f
beingborn <dolmin0708@naver.com>
2025-02-15 13:27:55 +0900

    project

---
+++ src/App.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="header">
+    <ul class="header-button-left">
+      <li>Cancel</li>
+    </ul>
+    <ul class="header-button-right">
+      <li>Next</li>
+    </ul>
+    <img src="./assets/logo.png" class="logo" />
+  </div>
+
+  <Container :PostData = "PostData" :step = "step" @update-step="updateStep"/>
+  <button @click="more()">데이터 가져오기</button>
+
+
+  <div class="footer">
+    <ul class="footer-button-plus">
+      <input type="file" id="file" class="inputfile" />
+      <label for="file" class="input-plus">+</label>
+    </ul>
+ </div>
+</template>
+
+<script>
+
+import Container from './components/Container.vue'
+import PostData from './assets/postData.js'
+import axios from 'axios'
+
+export default {
+  name : 'App',
+  components : {
+    Container : Container
+  },
+  data() {
+    return {
+      PostData : PostData,
+      moreCount : 0,
+      step : 0,
+    }
+  },
+  methods : {
+
+    // 전달 받은 매개변수를 새로운 데이터로 갈아치움
+    updateStep(newStep){
+      this.step = newStep;
+    },
+
+    more(){
+      axios.get(`https://codingapple1.github.io/vue/more${this.moreCount}.json`)
+        .then(res => {
+        this.PostData.unshift(res.data)
+        this.moreCount++
+      })
+      .catch((res)=> {
+        console.log('실패' , res)
+      })
+    }
+  },
+}
+</script>
+
+<style>
+  body {
+  margin: 0;
+}
+ul {
+  padding: 5px;
+  list-style-type: none;
+}
+.logo {
+  width: 22px;
+  margin: auto;
+  display: block;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 13px;
+}
+.header {
+  width: 100%;
+  height: 40px;
+  background-color: white;
+  padding-bottom: 8px;
+  position: sticky;
+  top: 0;
+}
+.header-button-left {
+  color: skyblue;
+  float: left;
+  width: 50px;
+  padding-left: 20px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+.header-button-right {
+  color: skyblue;
+  float: right;
+  width: 50px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+.footer {
+  width: 100%;
+  position: sticky;
+  bottom: 0;
+  padding-bottom: 10px;
+  background-color: white;
+}
+.footer-button-plus {
+  width: 80px;
+  margin: auto;
+  text-align: center;
+  cursor: pointer;
+  font-size: 24px;
+  padding-top: 12px;
+}
+.sample-box {
+  width: 100%;
+  height: 600px;
+  background-color: bisque;
+}
+.inputfile {
+  display: none;
+}
+.input-plus {
+  cursor: pointer;
+}
+#app {
+  box-sizing: border-box;
+  font-family: "consolas";
+  margin-top: 60px;
+  width: 100%;
+  max-width: 460px;
+  margin: auto;
+  position: relative;
+  border-right: 1px solid #eee;
+  border-left: 1px solid #eee;
+}
+</style>
---
+++ src/components/Container.vue
@@ -0,0 +1,100 @@
+<template>
+  <button @click="updateClick(0)">포스트</button>
+  <button @click="updateClick(1)">필터</button>
+  <button @click="updateClick(2)">글작성</button>
+
+  <div v-if="step == 0">
+    <Post :PostData ='PostData[i]' v-for="(post,i) in PostData" :key="i"/>
+  </div>
+  <!-- 필터선택페이지 -->
+   <div v-if="step == 1">  
+      <div class="upload-image"></div>
+      <div class="filters">
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+      </div>
+   </div>
+  
+
+
+  <!-- 글작성페이지 -->
+  <div v-if="step == 2">
+    <div class="upload-image"></div>
+      <div class="write">
+        <textarea class="write-box">write!</textarea>
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+import Post from './Post.vue'
+
+export default {
+  name: 'Container',
+  components : {
+    Post : Post
+  },
+  props: {
+    PostData : Array,
+    step : Number,
+  },
+
+  methods : {
+    updateClick(num){
+      this.$emit('update-step', num)
+    }
+  }
+
+
+}
+</script>
+
+<style>
+  .upload-image{
+width: 100%;
+height: 450px;
+background: cornflowerblue;
+background-size : cover;
+}
+.filters{
+overflow-x:scroll;
+white-space: nowrap;
+}
+.filter-1 {
+width: 100px;
+height: 100px;
+background-color: cornflowerblue;
+margin: 10px 10px 10px auto;
+padding: 8px;
+display: inline-block;
+color : white;
+background-size: cover;
+}
+.filters::-webkit-scrollbar {
+height: 5px;
+}
+.filters::-webkit-scrollbar-track {
+background: #f1f1f1; 
+}
+.filters::-webkit-scrollbar-thumb {
+background: #888; 
+border-radius: 5px;
+}
+.filters::-webkit-scrollbar-thumb:hover {
+background: #555; 
+}
+.write-box {
+  border: none;
+  width: 90%;
+  height: 100px;
+  padding: 15px;
+  margin: auto;
+  display: block;
+  outline: none;
+}
+</style>
